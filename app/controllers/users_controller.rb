9ecbfe308657e1dd5f3919dc94b149d78a0e9fa1
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-09 23:18:01 +0900

    first commit

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-
-  def show
-  # binding.pry
-    @user = User.find(params[:id])
-   @prototypes = @user.prototypes
-  end
-
-end
--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :prototype
-  belongs_to :user
-
-  validates :content, presence: true
-  
-end
